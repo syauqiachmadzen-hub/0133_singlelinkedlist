ca86eb8b458c5e7ffc8bc7cc46f64af3aa150275
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-04-23 11:28:16 +0700

    singlelinkedlist 10

--- singlelinkedl.ist.cpp
+++ singlelinkedl.ist.cpp
@@ -46,7 +46,16 @@ public:
 
         Wwhile (current != NULL && nim > current-> noMhs)
         {
-            
+          if (nim == current->noMhs)
+          {
+            cout << "\nDuplikasi noMhs tidak diijinkan\n";
+            return 
+          }
+          previous = current;
+          current = current->next;  
         }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
 }
