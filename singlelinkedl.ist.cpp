1ebb0e649754fab66c2ed336e072f0b3717f082d
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-04-23 11:43:02 +0700

    singlelinkedlist 11

--- singlelinkedl.ist.cpp
+++ singlelinkedl.ist.cpp
@@ -58,4 +58,31 @@ public:
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
+
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool search(int nim,node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previos = current;
+            curent = current->next;
+        }
+        return current !- NULL;
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim, previous, current))
+        return false;
+        
+    }
 }
